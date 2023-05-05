d7b59018e9a003f207132f0d7a5f2fc9573f42a7
0x7e3b <30737139+0x7e3b@users.noreply.github.com>
2023-05-05 17:23:43 +0200

    Removed empty lines

--- CreateWinPE.cmd
+++ CreateWinPE.cmd
@@ -1,7 +1,5 @@
 copype amd64 C:\WinPE_amd64_PS
-
 Dism /Mount-Image /ImageFile:"C:\WinPE_amd64_PS\media\sources\boot.wim" /Index:1 /MountDir:"C:\WinPE_amd64_PS\mount"
-
 Dism /Add-Package /Image:"C:\WinPE_amd64_PS\mount" /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-WMI.cab"
 Dism /Add-Package /Image:"C:\WinPE_amd64_PS\mount" /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\en-us\WinPE-WMI_en-us.cab"
 Dism /Add-Package /Image:"C:\WinPE_amd64_PS\mount" /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-NetFX.cab"
@@ -17,13 +15,10 @@ Dism /Add-Package /Image:"C:\WinPE_amd64_PS\mount" /PackagePath:"C:\Program File
 Dism /Add-Package /Image:"C:\WinPE_amd64_PS\mount" /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-SecureStartup.cab"
 Dism /Add-Package /Image:"C:\WinPE_amd64_PS\mount" /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\en-us\WinPE-SecureStartup_en-us.cab"
 Dism /Image:"C:\WinPE_amd64_PS\mount" /Set-InputLocale:de-DE
-ECHO ECHO off>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
+ECHO @echo off>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
 ECHO powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
 ECHO @for %%a in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do @if exist %%a:\Images\ set IMAGESDRIVE=%%a>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
 ECHO %IMAGESDRIVE%:>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
-
 Dism /Unmount-Image /MountDir:C:\WinPE_amd64_PS\mount /Commit
-
 MakeWinPEMedia /ISO C:\WinPE_amd64_PS %USERPROFILE%\Desktop\WinPE_amd64_PS.iso
-
 rmdir /S /Q C:\WinPE_amd64_PS
