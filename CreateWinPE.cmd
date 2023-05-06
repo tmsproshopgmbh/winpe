c5790f90ae7cebc4c94ff85bce06628a6ea67c0a
0x7e3b <30737139+0x7e3b@users.noreply.github.com>
2023-05-06 18:03:56 +0200

    Update CreateWinPE.cmd

--- CreateWinPE.cmd
+++ CreateWinPE.cmd
@@ -17,8 +17,8 @@ Dism /Add-Package /Image:"C:\WinPE_amd64_PS\mount" /PackagePath:"C:\Program File
 Dism /Image:"C:\WinPE_amd64_PS\mount" /Set-InputLocale:de-DE
 ECHO @echo off>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
 ECHO powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
-ECHO @for %%a in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do @if exist %%a:\Images\ set IMAGESDRIVE=%%a>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
-ECHO %IMAGESDRIVE%:>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
+ECHO @for %%%%a in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do @if exist %%%%a:\Images\ set IMAGESDRIVE=%%%%a>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
+ECHO %%IMAGESDRIVE%%:>>C:\WinPE_amd64_PS\mount\windows\system32\startnet.cmd
 Dism /Unmount-Image /MountDir:C:\WinPE_amd64_PS\mount /Commit
 call MakeWinPEMedia /ISO C:\WinPE_amd64_PS %USERPROFILE%\Desktop\WinPE_amd64_PS.iso
 rmdir /S /Q C:\WinPE_amd64_PS
